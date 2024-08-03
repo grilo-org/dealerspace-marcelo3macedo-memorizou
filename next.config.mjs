22a3234fec7b6004b960f27a60fca5a5bb241abc
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-08-03 15:46:37 -0300

    feat: adding intl configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from 'next-intl/plugin';
+import { promises as fs } from 'fs';
+import path from 'path';
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+async function getRewrites() {
+  const rewritesPath = path.join(process.cwd(), 'rewrites.json');
+  const data = await fs.readFile(rewritesPath, 'utf-8');
+  return JSON.parse(data);
+}
+
+const nextConfig = {
+  async rewrites() {
+    const rewrites = await getRewrites();
+    return rewrites;
+  },
+};
+
+export default withNextIntl(nextConfig);
