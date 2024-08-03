01d45a977c20da4f9f86aebe00ef405858eda752
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-08-03 15:47:40 -0300

    feat: adding institutional pages

---
+++ src/app/institutional/privacyPolicy/page.tsx
@@ -0,0 +1,17 @@
+import MultilineText from '@/components/common/multilineText';
+import Title from '@/components/common/title';
+import MainTheme from '@/themes/main.theme';
+import { useTranslations } from 'next-intl';
+
+export default function PrivacyPolicyPage() {
+  const t = useTranslations('privacyPolicy');
+
+  return (
+    <MainTheme>
+      <div className="container my-4 md:my-8 mx-auto px-2">
+        <Title>{t('title')}</Title>
+        <MultilineText>{t('description')}</MultilineText>
+      </div>
+    </MainTheme>
+  );
+}
