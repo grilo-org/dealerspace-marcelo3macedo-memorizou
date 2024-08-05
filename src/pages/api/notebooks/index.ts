f90ea0dba9538d549b99741b5c7fc1bd31a6a2c4
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-08-05 14:44:56 -0300

    feat: notebook endpoints

---
+++ src/pages/api/notebooks/index.ts
@@ -0,0 +1,32 @@
+import prisma from '@/lib/prisma';
+import { handleError } from '@/utils/errorHandler';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const pageNum = parseInt(page as string, 10);
+    const limitNum = parseInt(limit as string, 10);
+
+    const skip = (pageNum -1) * limitNum
+    const take = limitNum
+
+    const notebooks = await prisma.notebook.findMany({
+        where: {
+          deletedAt: null
+        },
+        skip,
+        take
+    })
+    const totalUsers = await prisma.notebook.count();
+
+    res.status(200).json({
+        data: notebooks,
+        total: totalUsers,
+        page: pageNum,
+        limit: limitNum,
+    });
+  } catch (error) {
+    handleError(error, res)
+  }
+}
---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,10 @@
+import { ZodError } from 'zod';
+import type { NextApiResponse } from 'next';
+
+export function handleError(error: unknown, res: NextApiResponse) {
+  if (error instanceof ZodError) {
+    return res.status(400).json({ error: error.errors });
+  }
+
+  return res.status(500).json({ error: 'Internal Server Error' });
+}
